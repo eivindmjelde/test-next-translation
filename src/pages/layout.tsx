317fa041aebb29643ce591758058c2d2a6e030fb
Eivind Mjelde <eivindmjelde@gmail.com>
2023-08-11 16:33:28 +0200

    bump

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -11,7 +11,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       className={` max-w-screen-xl mx-auto flex min-h-screen flex-col items-center gap-20 p-24 ${inter.className}`}
     >
       Current locale: {defaultLocale}
-      <p>ENDRING</p>
       {children}
       {defaultLocale?.includes("en") && <GoogleTranslate />}
     </main>
