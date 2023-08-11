448f872598d1f52326c4880b4eb02a58b94ed671
Eivind Mjelde <eivindmjelde@gmail.com>
2023-08-11 13:41:48 +0200

    changed 'locale' to 'defaultLocale'

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,12 +4,12 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { locale } = useRouter();
+  const { defaultLocale } = useRouter();
   return (
     <main
       className={` max-w-screen-xl mx-auto flex min-h-screen flex-col items-center gap-20 p-24 ${inter.className}`}
     >
-      Current locale: {locale}
+      Current locale: {defaultLocale}
       {children}
     </main>
   );
